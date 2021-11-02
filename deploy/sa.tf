452ea320f148e2b17918daa3810964e038986b37
Ulibka68 <gena68@inbox.ru>
2021-11-02 13:48:00 +0300

    сервисный аккаунт создан, ему назначены роли

--- deploy/sa.tf
+++ deploy/sa.tf
@@ -12,6 +12,7 @@ locals {
   srv_acc_catalog_id = yandex_iam_service_account.sa_catalog_test2.id
 }
 
+/*
 resource "yandex_iam_service_account_iam_binding" "ydb-admin" {
   service_account_id = local.srv_acc_catalog_id
   role               = "ydb.admin"
@@ -19,11 +20,35 @@ resource "yandex_iam_service_account_iam_binding" "ydb-admin" {
   members = [
     "serviceAccount:${local.srv_acc_catalog_id}",
   ]
+}*/
+
+
+resource "yandex_resourcemanager_folder_iam_binding" "ydb-admin" {
+  folder_id = local.folder_id
+  role = "ydb.admin"
+  members = ["serviceAccount:${local.srv_acc_catalog_id}",]
 }
 
-#yc resource-manager folder add-access-binding ${FOLDER_ID} --role ydb.admin --subject serviceAccount:${MOVIES_API_SA_ID}
-#yc resource-manager folder add-access-binding ${FOLDER_ID} --role container-registry.images.puller --subject serviceAccount:${MOVIES_API_SA_ID}
-#yc resource-manager folder add-access-binding ${FOLDER_ID} --role serverless.containers.invoker --subject serviceAccount:${MOVIES_API_SA_ID}
-#yc resource-manager folder add-access-binding ${FOLDER_ID} --role serverless.functions.invoker --subject serviceAccount:${MOVIES_API_SA_ID}
-#yc resource-manager folder add-access-binding ${FOLDER_ID} --role storage.editor --subject serviceAccount:${MOVIES_API_SA_ID}
 
+resource "yandex_resourcemanager_folder_iam_binding" "container-registry-images-puller" {
+  folder_id = local.folder_id
+  role = "container-registry.images.puller"
+  members = ["serviceAccount:${local.srv_acc_catalog_id}",]
+}
+
+resource "yandex_resourcemanager_folder_iam_binding" "serverless-containers-invoker" {
+  folder_id = local.folder_id
+  role = "serverless.containers.invoker"
+  members = ["serviceAccount:${local.srv_acc_catalog_id}",]
+}
+resource "yandex_resourcemanager_folder_iam_binding" "serverless-functions-invoker" {
+  folder_id = local.folder_id
+  role = "serverless.functions.invoker"
+  members = ["serviceAccount:${local.srv_acc_catalog_id}",]
+}
+
+resource "yandex_resourcemanager_folder_iam_binding" "storage-editor" {
+  folder_id = local.folder_id
+  role = "storage.editor"
+  members = ["serviceAccount:${local.srv_acc_catalog_id}",]
+}
